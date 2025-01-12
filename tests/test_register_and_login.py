ab98421779c25e7df44992cf0ae79d211da26bd5
AnnaAQA <kalinina_anna_spb@mail.ru>
2025-01-12 14:49:42 +0300

    add headers to get_user_data

--- tests/test_register_and_login.py
+++ tests/test_register_and_login.py
@@ -16,26 +16,30 @@ user_data_only_email = json.load(json_file_user_data_only_email)
 
 @pytest.mark.parametrize('users_data', users_data)
 def test_successful_register(users_data):
-    response = httpx.post(BASE_URL+REGISTER_USER, json=users_data)
+    headers = {'Content-type': 'application/json'}
+    response = httpx.post(BASE_URL+REGISTER_USER, json=users_data, headers=headers)
     assert response.status_code == 200
 
     validate(response.json(), REGISTERED_USER_SCHEME)
 
 @pytest.mark.parametrize('user_data_only_email', user_data_only_email)
 def test_unsuccessful_register(user_data_only_email):
-    response = httpx.post(BASE_URL + REGISTER_USER, json=user_data_only_email)
+    headers = {'Content-type': 'application/json'}
+    response = httpx.post(BASE_URL + REGISTER_USER, json=user_data_only_email, headers=headers)
     assert response.status_code == 400
 
 @pytest.mark.parametrize('users_data', users_data)
 def test_successful_login(users_data):
-    response = httpx.post(BASE_URL+LOGIN_USER, json=users_data)
+    headers = {'Content-type': 'application/json'}
+    response = httpx.post(BASE_URL+LOGIN_USER, json=users_data, headers=headers)
     assert response.status_code == 200
 
     validate(response.json(), LOGGEDIN_USER_SCHEME)
 
 @pytest.mark.parametrize('user_data_only_email', user_data_only_email)
 def test_unsuccessful_login(user_data_only_email):
-    response = httpx.post(BASE_URL+LOGIN_USER, json=user_data_only_email)
+    headers = {'Content-type': 'application/json'}
+    response = httpx.post(BASE_URL+LOGIN_USER, json=user_data_only_email, headers=headers)
     assert response.status_code == 400
 
 
